a1e36a09d7ba25762edd66fccb701d525a1bc5ad
Giorgio Audrito <giorgio.audrito@gmail.com>
2021-06-30 18:57:49 +0200

    updating to new build system

--- make.sh
+++ make.sh
@@ -2,55 +2,28 @@
 
 if [ "$1" == "" ]; then
     echo -e "\033[4musage:\033[0m"
-    echo -e "    \033[1m./make.sh [platform] [target...]\033[0m"
-    echo -e "you can specify \"all\" as target to make all targets."
+    echo -e "    \033[1m./make.sh [platform] target...\033[0m"
+    echo
+    echo -e "platform can be windows or unix and may be omitted for automatic detection."
+    echo
+    echo -e "available targets are:"
+    for f in run/*.cpp; do
+        g=`basename $f .cpp`
+        echo -en "    \033[1m$g\033[0m"
+    done
+    echo
+    echo -e "you can also specify \033[1mall\033[0m as target to make all targets."
+    echo -e "Targets can be substrings demanding builds for all possible expansions."
     exit 1
 fi
-
-platform=$1
-if [ "$platform" == windows ]; then
-    flag=MinGW
-elif [ "$platform" == unix ]; then
-    flag=Unix
-else
-    echo -e "\033[4mUnrecognized platform \"$platform\". Available platforms are:\033[0m"
-    echo -e "    \033[1mwindows unix\033[0m"
-    exit 1
-fi
-shift 1
-
-function make_target {
-    target=$1
-    if [ ! -f "run/$target.cpp" ]; then
-        echo -e "\033[4mUnrecognized target \"$target\".\033[0m"
-        exit 1
-    fi
-    cd bin
-    ./$target | tee ../plot/$target.asy
-    cd ../plot
-    asy $target.asy -f pdf
-    rm *timfail*
-    cd ..
-}
 
 git submodule init
 git submodule update
-cmake -S ./ -B ./bin -G "$flag Makefiles" -DCMAKE_BUILD_TYPE=Release
-if [ $? -ne 0 ]; then
-    echo -e "\033[4mCMake error, exiting...\033[0m"
-    exit 1
-fi
-cmake --build ./bin/
-if [ $? -ne 0 ]; then
-    echo -e "\033[4mCompile error, exiting...\033[0m"
-    exit 1
-fi
-if [ "$1" == "all" ]; then
-    for target in crowd_safety drones_recognition service_discovery smart_home; do
-        make_target $target
-    done
+platform=$1
+if [ "$platform" == windows -o "$platform" == unix ]; then
+    shift 1
 else
-    for target in "$@"; do
-        make_target $target
-    done
+    platform=""
 fi
+fcpp/src/make.sh $platform gui run -O "$@"
+rm -rf plot/*timfail* output/raw
